c6f91b1a15a49d2343c5c41709304114f09a34bc
YURAa2525 <yut2094vb@ezweb.ne.jp>
2025-12-28 02:39:07 +0900

    a

--- script/top.js
+++ script/top.js
@@ -7,37 +7,56 @@ function createBubble() {
   const $bubbles = $(".js-bubbles");
   const maxSize  = 16;
   const minSize  = 8;
+  let   bubblesWidth;
+  let   windowHeight;
 
+  
+  getBubblesSize();
   createBubbleCore();
 
 
-  // ★ 生成位置 y は、常に最下ではなく、現在ｳｲﾝﾄﾞｳの下部からにする
-  //    → そうしないと、最下から生成してもﾌｧｰｽﾄﾋﾞｭｰに描画されるまで結構時間を要するから
+  $(window).on("resize", () => {
+    getBubblesSize();
+  });
+
+
+  function getBubblesSize() {
+    bubblesWidth = $bubbles.width();
+    windowHeight = $(window).innerHeight();
+  }
+
+
+
+
 
   // ★ ﾏｳｽを動かしたり、どこかをｸﾘｯｸした時、その時点から 泡 を生成する
   //    →ｸﾘｯｸはｽﾏﾎ、動かしorｸﾘｯｸはpc
-  //    これで生成された 泡 は、初速は速くし、複数の 泡 が散らばるように登場させる
+  //    これで生成された 泡 は、初速は速くし、複数の 泡 が散らばる (ﾗﾝﾀﾞﾑに) ように登場させる
 
   // ★ 生成された 泡 は、少し左右に揺れながら上昇し（←確定）
   //                    ﾗﾝﾀﾞﾑ秒後に消滅する（←検討中）
 
   function createBubbleCore() {
-    // 生成に必要な値をﾗﾝﾀﾞﾑで取得
     const setSize       = (Math.random() * (maxSize - minSize) + minSize).toFixed(2);
-    const setPosX       = (Math.random() * ($bubbles.width() - 0) + 0).toFixed(2);
-    const setDuration   = (Math.random() * (30 - 15) + 15).toFixed(2);
+    const setPosX       = (Math.random() * (bubblesWidth - 0) + 0).toFixed(2);
+    const setDuration = (Math.random() * (30 - 15) + 15).toFixed(2);
     const setInterval   = (Math.random() * (4 - 0) + 0).toFixed(2);
+
     // setSize に応じて 0.8 ~ 1.0 の値を算出
     const setBrightness = (0.8 + (setSize - minSize) * (0.2 / minSize)).toFixed(2);
 
+    // 生成位置 y は、現ｳｲﾝﾄﾞｳの下部
+    const setPosY = $(window).scrollTop() + windowHeight;
+    
     // 取得したﾗﾝﾀﾞﾑ値を持つ js-bubble を生成し js-bubbles に追加
     const $bubble = $("<span>")
       .addClass("bubble js-bubbles")
       .css({
         "width": setSize + "px",
         "height": setSize + "px",
+        "top": setPosY + "px",
         "left" : setPosX + "px",
-        "animation-duration": setDuration + "s",
+        "animation": `floating ${setDuration}s linear 1 forwards`,
         "filter": `brightness(${setBrightness})`,
       });
     $bubbles.append($bubble);
@@ -48,6 +67,8 @@ function createBubble() {
     }, setDuration * 1000);
 
     // 次回の生成を予約
-    setTimeout(createBubbleCore, setInterval * 1000);
+    setTimeout(() => {
+      createBubbleCore("auto");
+    }, setInterval * 1000);
   }
 }
