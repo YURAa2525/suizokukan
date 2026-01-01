77e356c24b3c082a70a634651a85d48299837a33
YURAa2525 <yut2094vb@ezweb.ne.jp>
2026-01-02 01:54:08 +0900

    a

--- script/top.js
+++ script/top.js
@@ -1,8 +1,11 @@
+let g_userDevice = "mobile";
+
 $(function() {
+  if (window.matchMedia("(pointer: fine)").matches) g_userDevice = "pc";
+
   mainMarginTop();
   // createBubble();
-  mvSlide();
-
+  slideManage();
 
   // --------------------------------------------
   // js-main の margin-top 値を js-header の高さに合わせる
@@ -105,6 +108,93 @@ function createBubble() {
 }
 
 
-function mvSlide() {
+// ----------------------------------------------
+// 
+// > ﾓﾊﾞｲﾙ端末では次矢印のｸﾘｯｸ及び横ｽｸﾛｰﾙ、pc端末では次矢印のｸﾘｯｸのみで発火する
+// ----------------------------------------------
+function slideManage() {
+  const slideAnimDuration  = 21.6;
+  const slideAnimName      = "fade-in-out";
+  const slideFadeinoutTime = 0.4;
 
+  const slideCnt       = $(".js-slide").length;
+  const maxSlideIx     = slideCnt - 1;
+  const maxZIndex      = maxSlideIx;
+  let   showSlideIx    = 0;
+  let   isWait         = false;
+  let   slideDelayList = [];
+
+  // 総再生時間、ｽﾗｲﾄﾞ枚数、変化時間、から keyframes を作成
+  const $style = $("<style>").text(`
+    @keyframes ${slideAnimName} {
+      from { opacity: 1; }
+      ${(slideAnimDuration / slideCnt - slideFadeinoutTime) / slideAnimDuration * 100}% { opacity: 1; }
+      ${slideAnimDuration / slideCnt / slideAnimDuration * 100}% { opacity: 0; }
+      ${(slideAnimDuration - slideFadeinoutTime) / slideAnimDuration * 100}% { opacity: 0 }
+      to { opacity: 1; }
+    }
+    `);
+  $("head").append($style);
+
+  // 総再生時間 (slideAnimDuration) から、ｽﾗｲﾄﾞ1枚ごとの遅延時間を取得
+  const delay = slideAnimDuration / slideCnt;
+  for (let i = 0; i <= maxSlideIx; i++) {
+    slideDelayList.push(delay * i);
+  }
+
+  // js-slide の順番ごとに遅延時間を設定
+  $(".js-slide").each(function(i) {
+    $(this).css({
+      "z-index"        : maxZIndex - i,
+      "animation" : `${slideAnimName} ${slideAnimDuration}s linear ${slideDelayList[i]}s infinite`,
+    });
+  });
+
+  // ﾓﾊﾞｲﾙ端末の場合のみ横ｽｸﾛｰﾙで発火
+  if (g_userDevice == "mobile") {
+    let touchStartX = 0;
+    let touchStartY = 0;
+
+    $(".slider").on("touchstart", function(eStart) {
+      isWait = false;
+      touchStartX = eStart.touchs[0].clientX;
+      touchStartY = eStart.touchs[0].clientY;
+    });
+
+    $(".slider").on("touchmove", function(eMove) {
+      if (isWait) return;
+      isWait = true;
+
+      const diffX = eMove.touches[0].clientX - touchStartX;
+      const diffY = eMove.touches[0].clientY - touchStartY;
+
+      // 横ｽｸﾛｰﾙ量 < 縦ｽｸﾛｰﾙ量 の場合は、縦ｽｸﾛｰﾙとみなし処理しない
+      if (Math.abs(diffX) < Math.abs(diffY)) return;
+      
+      if (diffX < 0) {
+        showSlideIx = Math.min(showSlideIx + 1, maxSlideIx);
+      }
+      else {
+        showSlideIx = Math.max(showSlideIx - 1, 0);
+      }
+    });
+  }
+
+  $(".mv").on("click", ".js-arrow", function() {
+    if (isWait) return;
+    isWait = true;
+
+    const arrowDire = $(this).data("arrow");
+    if (arrowDire == "left") {
+      showSlideIx = Math.min(showSlideIx + 1, maxSlideIx);
+    }
+    else if (arrowDire == "right") {
+      showSlideIx = Math.max(showSlideIx - 1, 0);
+    }
+  });
+
+
+  function slideItem() {
+
+  }
 }
